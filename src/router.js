71daf43498e96a10cc431919953caac9ae6f7866
Aden <adeziio@yahoo.com>
2021-10-05 02:43:37 -0400

    put back POST to have more options

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ const Data = require('./data');
 const router = express.Router();
 const jsonParser = express.json();
 
+// GET
 router.get('/list', (req, res) => {
     Data.all((err, todos) => {
         res.status(200).json(todos)
@@ -19,7 +20,7 @@ router.get('/add', jsonParser, (req, res) => {
     else {
         res.json({
             status: "Failed",
-            message: `Please provide a parameter value for the key item`
+            message: "Please provide a parameter value for the key item"
         })
     }
 })
@@ -52,22 +53,98 @@ router.get('/delete', jsonParser, (req, res) => {
     else {
         res.json({
             status: "Failed",
-            message: `Please provide a parameter value for the key id or item`
+            message: "Please provide a parameter value for the key id or item"
+        })
+    }
+})
+
+router.get('/update', jsonParser, (req, res) => {
+    if (req.query.id && req.query.item) {
+        Data.update(req.query.id, req.query.item, (err, data) => {
+            if (err) {
+                res.status(404, "Id or Item not found").send();
+            }
+            else {
+                res.json({
+                    status: "Success"
+                })
+            }
+        });
+    }
+    else {
+        res.json({
+            status: "Failed",
+            message: "Missing values for id or item"
+        })
+    }
+})
+
+// POST
+router.post('/add', jsonParser, (req, res) => {
+    if (req.body.item) {
+        Data.add(req.body.item);
+        res.json({
+            status: "Success"
+        });
+    }
+    else {
+        res.json({
+            status: "Failed",
+            message: "Please provide a parameter value for the key item"
+        })
+    }
+})
+
+router.post('/delete', jsonParser, (req, res) => {
+    if (req.body.id) {
+        Data.deleteById(req.body.id, (err) => {
+            if (err) {
+                res.status(404, "Id not found").send();
+            } else {
+                res.json({
+                    status: "Success"
+                })
+            }
+        });
+    }
+    else if (req.body.item) {
+        Data.deleteByItem(req.body.item, (err) => {
+            if (err) {
+                res.status(404, "Item not found").send();
+            } else {
+                res.json({
+                    status: "Success"
+                })
+            }
+        });
+    }
+    else {
+        res.json({
+            status: "Failed",
+            message: "Please provide a parameter value for the key id or item"
         })
     }
 })
 
 router.post('/update', jsonParser, (req, res) => {
-    Data.update(req.body.id, req.body.item, (err, data) => {
-        if (err) {
-            res.status(404, "Id or Item not found").send();
-        }
-        else {
-            res.json({
-                status: "Success"
-            })
-        }
-    });
+    if (req.body.id && req.body.item) {
+        Data.update(req.body.id, req.body.item, (err, data) => {
+            if (err) {
+                res.status(404, "Id or Item not found").send();
+            }
+            else {
+                res.json({
+                    status: "Success"
+                })
+            }
+        });
+    }
+    else {
+        res.json({
+            status: "Failed",
+            message: "Missing values for id or item"
+        })
+    }
 })
 
 module.exports = router;
